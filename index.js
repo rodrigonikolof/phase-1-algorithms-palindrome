71e2f653f9c608419c432adb043a81f84a5ac984
Rodrigo Nikolof <rodrigo.nikolof@outlook.com>
2022-11-05 13:18:26 +1100

    Done with assignment

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 function isPalindrome(word) {
-  // Write your algorithm here
-}
+  
+  let reverseWords = word.split('').reverse().join('')
 
+return reverseWords === word ? true : false
+}
+isPalindrome('ouch')
 /* 
   Add your pseudocode here
 */
@@ -11,15 +14,15 @@ function isPalindrome(word) {
 */
 
 // You can run `node index.js` to view these console logs
-if (require.main === module) {
-  // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
+// if (require.main === module) {
+//   // add your own custom tests in here
+//   console.log("Expecting: true");
+//   console.log("=>", isPalindrome("racecar"));
 
-  console.log("");
+//   console.log("");
 
-  console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
-}
+//   console.log("Expecting: false");
+//   console.log("=>", isPalindrome("robot"));
+// }
 
 module.exports = isPalindrome;
